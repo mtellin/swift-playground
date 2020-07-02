4a5f0340bc7c7f9a29490a93a26c9145853df184
Mike Tellinghuisen <mtellin@gmail.com>
2020-07-02 18:09:44 +0000

    possible fix for string interpolation error

--- api-open-weather.swift
+++ api-open-weather.swift
@@ -8,12 +8,7 @@ struct WeatherManager {
     let cityName = "minneapolis"
     // get env value for API Key
     let apiKey = ProcessInfo.processInfo.environment["OPEN_WEATHER_API_KEY"]
-
-    /*
-    Causes this error:
-    string interpolation produces a debug description for an optional value; did you mean to make this explicit?
-
-    */
+    
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial"
 
     func fetchWeather(cityName: String) {
@@ -21,9 +16,12 @@ struct WeatherManager {
         TODO: 
         string interpolation produces a debug description for an optional value; did you mean to make this explicit
 
+        FIX? Used \(String(describing: apiKey)) instead and it seems to not produce the error now. That was the Fix from Xcode
+
         */
-       let urlString = "\(weatherURL)&q=\(cityName)&appid=\(apiKey)"
-        performRequest(urlString: urlString)
+       let urlString = "\(weatherURL)&q=\(cityName)&appid=\(String(describing: apiKey))"
+
+       performRequest(urlString: urlString)
     }
 
         func performRequest(urlString: String) {
