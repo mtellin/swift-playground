304cae5f692e3d3a0014fb0aef2e0f9efe17260e
Mike Tellinghuisen <mtellin@gmail.com>
2020-07-05 22:36:30 -0500

    possible working challenge solution but need to check if case works

--- swift-coding-challenges/01-challenge.swift
+++ swift-coding-challenges/01-challenge.swift
@@ -9,10 +9,27 @@ var temp = [String]()
 
 for i in 0..<characters.count {
     print(characters[i])
+    print(type(of: characters[i]))
+// Have to convert the type Character to String value to use .contains
+    if temp.contains(String(characters[i])) {
+      print("\(temp) contains \(characters[i]).")
+    } else {
+      temp.append(String(characters[i]))
+    }
+print("temp contains: \(temp)")
+
+/*
+    if temp.contains(characters[i]) {
+      print("yes")
+    }
+*/
+
+/*
+thought i was on to something here but seems like i cant use .map on a string
     if temp.contains(characters[i].map(String.init)) == false {
         print("dup found")
     }
-    
+ */   
     /* for j in 0..<temp.count {
         if string[i] != temp[j] {
             //string[i] += temp
